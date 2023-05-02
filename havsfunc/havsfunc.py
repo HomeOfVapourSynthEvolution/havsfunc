1d2117d5c3795a243b1c70262dd5b1dbade88daa
HolyWu <holywu@gmail.com>
2023-05-03 00:22:48 +0800

    Outdate GrainFactory3

--- havsfunc/havsfunc.py
+++ havsfunc/havsfunc.py
@@ -24,7 +24,6 @@ __all__ = [
     "GSMC",
     "MCTemporalDenoise",
     "STPresso",
-    "GrainFactory3",
     "InterFrame",
     "SmoothLevels",
     "FastLineDarkenMOD",
@@ -3249,111 +3248,8 @@ def bbmod(*args, **kwargs):
     raise vs.Error("havsfunc.bbmod outdated. Use https://github.com/OpusGang/awsmfunc instead.")
 
 
-# Parameters:
-#  g1str (float)       - [0.0 - ???] strength of grain / for dark areas. Default is 7.0
-#  g2str (float)       - [0.0 - ???] strength of grain / for midtone areas. Default is 5.0
-#  g3str (float)       - [0.0 - ???] strength of grain / for bright areas. Default is 3.0
-#  g1shrp (int)        - [0 - 100] sharpness of grain / for dark areas (NO EFFECT when g1size=1.0 !!). Default is 60
-#  g2shrp (int)        - [0 - 100] sharpness of grain / for midtone areas (NO EFFECT when g2size=1.0 !!). Default is 66
-#  g3shrp (int)        - [0 - 100] sharpness of grain / for bright areas (NO EFFECT when g3size=1.0 !!). Default is 80
-#  g1size (float)      - [0.5 - 4.0] size of grain / for dark areas. Default is 1.5
-#  g2size (float)      - [0.5 - 4.0] size of grain / for midtone areas. Default is 1.2
-#  g3size (float)      - [0.5 - 4.0] size of grain / for bright areas. Default is 0.9
-#  temp_avg (int)      - [0 - 100] percentage of noise's temporal averaging. Default is 0
-#  ontop_grain (float) - [0 - ???] additional grain to put on top of prev. generated grain. Default is 0.0
-#  seed (int)          - specifies a repeatable grain sequence. Set to at least 0 to use.
-#  th1 (int)           - start of dark->midtone mixing zone. Default is 24
-#  th2 (int)           - end of dark->midtone mixing zone. Default is 56
-#  th3 (int)           - start of midtone->bright mixing zone. Default is 128
-#  th4 (int)           - end of midtone->bright mixing zone. Default is 160
-def GrainFactory3(clp, g1str=7.0, g2str=5.0, g3str=3.0, g1shrp=60, g2shrp=66, g3shrp=80, g1size=1.5, g2size=1.2, g3size=0.9, temp_avg=0, ontop_grain=0.0, seed=-1, th1=24, th2=56, th3=128, th4=160):
-    if not isinstance(clp, vs.VideoNode):
-        raise vs.Error('GrainFactory3: this is not a clip')
-
-    if clp.format.color_family == vs.RGB:
-        raise vs.Error('GrainFactory3: RGB format is not supported')
-
-    if clp.format.sample_type == vs.INTEGER:
-        neutral = 1 << (clp.format.bits_per_sample - 1)
-        peak = (1 << clp.format.bits_per_sample) - 1
-    else:
-        neutral = 0.0
-        peak = 1.0
-
-    if clp.format.color_family != vs.GRAY:
-        clp_orig = clp
-        clp = plane(clp, 0)
-    else:
-        clp_orig = None
-
-    ox = clp.width
-    oy = clp.height
-    sx1 = m4(ox / g1size)
-    sy1 = m4(oy / g1size)
-    sx1a = m4((ox + sx1) / 2)
-    sy1a = m4((oy + sy1) / 2)
-    sx2 = m4(ox / g2size)
-    sy2 = m4(oy / g2size)
-    sx2a = m4((ox + sx2) / 2)
-    sy2a = m4((oy + sy2) / 2)
-    sx3 = m4(ox / g3size)
-    sy3 = m4(oy / g3size)
-    sx3a = m4((ox + sx3) / 2)
-    sy3a = m4((oy + sy3) / 2)
-
-    b1 = g1shrp / -50 + 1
-    b2 = g2shrp / -50 + 1
-    b3 = g3shrp / -50 + 1
-    b1a = b1 / 2
-    b2a = b2 / 2
-    b3a = b3 / 2
-    c1 = (1 - b1) / 2
-    c2 = (1 - b2) / 2
-    c3 = (1 - b3) / 2
-    c1a = (1 - b1a) / 2
-    c2a = (1 - b2a) / 2
-    c3a = (1 - b3a) / 2
-    tmpavg = temp_avg / 100
-    th1 = scale(th1, peak)
-    th2 = scale(th2, peak)
-    th3 = scale(th3, peak)
-    th4 = scale(th4, peak)
-
-    grainlayer1 = clp.std.BlankClip(width=sx1, height=sy1, color=[neutral]).grain.Add(var=g1str, seed=seed)
-    if g1size != 1 and (sx1 != ox or sy1 != oy):
-        if g1size > 1.5:
-            grainlayer1 = grainlayer1.resize.Bicubic(sx1a, sy1a, filter_param_a=b1a, filter_param_b=c1a).resize.Bicubic(ox, oy, filter_param_a=b1a, filter_param_b=c1a)
-        else:
-            grainlayer1 = grainlayer1.resize.Bicubic(ox, oy, filter_param_a=b1, filter_param_b=c1)
-
-    grainlayer2 = clp.std.BlankClip(width=sx2, height=sy2, color=[neutral]).grain.Add(var=g2str, seed=seed)
-    if g2size != 1 and (sx2 != ox or sy2 != oy):
-        if g2size > 1.5:
-            grainlayer2 = grainlayer2.resize.Bicubic(sx2a, sy2a, filter_param_a=b2a, filter_param_b=c2a).resize.Bicubic(ox, oy, filter_param_a=b2a, filter_param_b=c2a)
-        else:
-            grainlayer2 = grainlayer2.resize.Bicubic(ox, oy, filter_param_a=b2, filter_param_b=c2)
-
-    grainlayer3 = clp.std.BlankClip(width=sx3, height=sy3, color=[neutral]).grain.Add(var=g3str, seed=seed)
-    if g3size != 1 and (sx3 != ox or sy3 != oy):
-        if g3size > 1.5:
-            grainlayer3 = grainlayer3.resize.Bicubic(sx3a, sy3a, filter_param_a=b3a, filter_param_b=c3a).resize.Bicubic(ox, oy, filter_param_a=b3a, filter_param_b=c3a)
-        else:
-            grainlayer3 = grainlayer3.resize.Bicubic(ox, oy, filter_param_a=b3, filter_param_b=c3)
-
-    expr1 = f'x {th1} < 0 x {th2} > {peak} {peak} {th2 - th1} / x {th1} - * ? ?'
-    expr2 = f'x {th3} < 0 x {th4} > {peak} {peak} {th4 - th3} / x {th3} - * ? ?'
-    grainlayer = core.std.MaskedMerge(core.std.MaskedMerge(grainlayer1, grainlayer2, clp.std.Expr(expr=[expr1])), grainlayer3, clp.std.Expr(expr=[expr2]))
-
-    if temp_avg > 0:
-        grainlayer = core.std.Merge(grainlayer, AverageFrames(grainlayer, weights=[1] * 3), weight=[tmpavg])
-    if ontop_grain > 0:
-        grainlayer = grainlayer.grain.Add(var=ontop_grain, seed=seed)
-
-    result = core.std.MakeDiff(clp, grainlayer)
-
-    if clp_orig is not None:
-        result = core.std.ShufflePlanes([result, clp_orig], planes=[0, 1, 2], colorfamily=clp_orig.format.color_family)
-    return result
+def GrainFactory3(*args, **kwargs):
+    raise vs.Error("havsfunc.GrainFactory3 outdated. Use https://github.com/Irrational-Encoding-Wizardry/vs-deband instead.")
 
 
 #------------------------------------------------------------------------------#
