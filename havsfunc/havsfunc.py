fd72302e96bd8814eb78ebe2f9ffc1d306a22ac5
HolyWu <holywu@gmail.com>
2023-05-07 13:17:27 +0800

    Outdate Gauss

--- havsfunc/havsfunc.py
+++ havsfunc/havsfunc.py
@@ -6,7 +6,7 @@ from functools import partial
 from typing import Any, Mapping, Optional, Sequence, Union
 
 from vsexprtools import norm_expr
-from vsrgtools import repair
+from vsrgtools import gauss_blur, repair
 from vsrgtools.util import mean_matrix, wmean_matrix
 from vstools import (
     DitherType,
@@ -975,7 +975,7 @@ def QTGMC(
         if SrchClipPP == 1:
             spatialBlur = repair0.resize.Bilinear(w // 2, h // 2).std.Convolution(matrix=matrix, planes=CMplanes).resize.Bilinear(w, h)
         elif SrchClipPP >= 2:
-            spatialBlur = Gauss(repair0.std.Convolution(matrix=matrix, planes=CMplanes), p=2.35)
+            spatialBlur = gauss_blur(repair0.std.Convolution(matrix=matrix, planes=CMplanes), 1.75)
             spatialBlur = core.std.Merge(spatialBlur, repair0, weight=0.1 if ChromaMotion or is_gray else [0.1, 0])
         if SrchClipPP <= 0:
             srchClip = repair0
@@ -1297,7 +1297,7 @@ def QTGMC(
     if Sbb not in [1, 3]:
         backBlend1 = thin
     else:
-        backBlend1 = core.std.MakeDiff(thin, Gauss(core.std.MakeDiff(thin, lossed1, planes=0).std.Convolution(matrix=matrix, planes=0), p=5), planes=0)
+        backBlend1 = core.std.MakeDiff(thin, gauss_blur(core.std.MakeDiff(thin, lossed1, planes=0).std.Convolution(matrix=matrix, planes=0), 1.2), planes=0)
 
     # Limit over-sharpening by clamping to neighboring (spatial or temporal) min/max values in original
     # Occurs here (before final temporal smooth) if SLMode == 1,2. This location will restrict sharpness more, but any artefacts introduced will be smoothed
@@ -1316,7 +1316,7 @@ def QTGMC(
         backBlend2 = sharpLimit1
     else:
         backBlend2 = core.std.MakeDiff(
-            sharpLimit1, Gauss(core.std.MakeDiff(sharpLimit1, lossed1, planes=0).std.Convolution(matrix=matrix, planes=0), p=5), planes=0
+            sharpLimit1, gauss_blur(core.std.MakeDiff(sharpLimit1, lossed1, planes=0).std.Convolution(matrix=matrix, planes=0), 1.2), planes=0
         )
 
     # Add back any extracted noise, prior to final temporal smooth - this will restore detail that was removed as "noise" without restoring the noise itself
@@ -4219,42 +4219,8 @@ def ChangeFPS(*args, **kwargs):
     raise vs.Error("havsfunc.ChangeFPS outdated. Use https://github.com/Irrational-Encoding-Wizardry/vs-tools instead.")
 
 
-def Gauss(clip: vs.VideoNode, p: Optional[float] = None, sigma: Optional[float] = None, planes: Optional[Union[int, Sequence[int]]] = None) -> vs.VideoNode:
-    if not isinstance(clip, vs.VideoNode):
-        raise vs.Error('Gauss: this is not a clip')
-
-    if p is None and sigma is None:
-        raise vs.Error('Gauss: must have p or sigma')
-
-    if p is not None and not 0.385 <= p <= 64.921:
-        raise vs.Error('Gauss: p must be between 0.385 and 64.921 (inclusive)')
-
-    if sigma is not None and not 0.334 <= sigma <= 4.333:
-        raise vs.Error('Gauss: sigma must be between 0.334 and 4.333 (inclusive)')
-
-    if sigma is None and p is not None:
-        # Translate AviSynth parameter to standard parameter.
-        sigma = math.sqrt(1.0 / (2.0 * (p / 10.0) * math.log(2)))
-
-    # 6 * sigma + 1 rule-of-thumb.
-    taps = int(math.ceil(sigma * 6 + 1))
-    if not taps % 2:
-        taps += 1
-
-    # Gaussian kernel.
-    kernel = []
-    for x in range(int(math.floor(taps / 2))):
-        kernel.append(1.0 / (math.sqrt(2.0 * math.pi) * sigma) * math.exp(-(x * x) / (2 * sigma * sigma)))
-
-    # Renormalize to -1023...1023.
-    for i in range(1, len(kernel)):
-        kernel[i] *= 1023 / kernel[0]
-    kernel[0] = 1023
-
-    # Symmetry.
-    kernel = kernel[::-1] + kernel[1:]
-
-    return clip.std.Convolution(matrix=kernel, planes=planes, mode='hv')
+def Gauss(*args, **kwargs):
+    raise vs.Error("havsfunc.Gauss outdated. Use https://github.com/Irrational-Encoding-Wizardry/vs-rgtools instead.")
 
 
 def mt_clamp(
