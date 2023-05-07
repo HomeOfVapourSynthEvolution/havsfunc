17b62a0b2695e0950e0899dba466ab42327c32c9
HolyWu <holywu@gmail.com>
2023-05-07 11:43:15 +0800

    Outdate santiag

--- havsfunc/havsfunc.py
+++ havsfunc/havsfunc.py
@@ -12,7 +12,6 @@ __all__ = [
     "daa",
     "daa3mod",
     "mcdaa3",
-    "santiag",
     "FixChromaBleedingMod",
     "Deblock_QED",
     "QTGMC",
@@ -90,106 +89,8 @@ def mcdaa3(clip: vs.VideoNode, opencl: bool = False, device: int | None = None, 
     return clip.std.MaskedMerge(csaa, momask)
 
 
-def santiag(
-    c: vs.VideoNode,
-    strh: int = 1,
-    strv: int = 1,
-    type: str = 'nnedi3',
-    nsize: Optional[int] = None,
-    nns: Optional[int] = None,
-    qual: Optional[int] = None,
-    pscrn: Optional[int] = None,
-    int16_prescreener: Optional[bool] = None,
-    int16_predictor: Optional[bool] = None,
-    exp: Optional[int] = None,
-    aa: Optional[int] = None,
-    alpha: Optional[float] = None,
-    beta: Optional[float] = None,
-    gamma: Optional[float] = None,
-    nrad: Optional[int] = None,
-    mdis: Optional[int] = None,
-    vcheck: Optional[int] = None,
-    fw: Optional[int] = None,
-    fh: Optional[int] = None,
-    halfres: bool = False,
-    typeh: Optional[str] = None,
-    typev: Optional[str] = None,
-    opencl: bool = False,
-    device: Optional[int] = None,
-) -> vs.VideoNode:
-    '''
-    santiag v1.6
-    Simple antialiasing
-
-    type = "nnedi3", "eedi2", "eedi3" or "sangnom"
-    '''
-
-    def santiag_dir(c: vs.VideoNode, strength: int, type: str, fw: Optional[int] = None, fh: Optional[int] = None) -> vs.VideoNode:
-        fw = fallback(fw, c.width)
-        fh = fallback(fh, c.height)
-
-        c = santiag_stronger(c, strength, type)
-
-        return c.resize.Spline36(fw, fh, src_top=0 if halfres else 0.5)
-
-    def santiag_stronger(c: vs.VideoNode, strength: int, type: str) -> vs.VideoNode:
-        if opencl:
-            nnedi3 = partial(core.nnedi3cl.NNEDI3CL, nsize=nsize, nns=nns, qual=qual, pscrn=pscrn, device=device)
-            eedi3 = partial(core.eedi3m.EEDI3CL, alpha=alpha, beta=beta, gamma=gamma, nrad=nrad, mdis=mdis, vcheck=vcheck, device=device)
-        else:
-            nnedi3 = partial(
-                core.znedi3.nnedi3, nsize=nsize, nns=nns, qual=qual, pscrn=pscrn, int16_prescreener=int16_prescreener, int16_predictor=int16_predictor, exp=exp
-            )
-            eedi3 = partial(core.eedi3m.EEDI3, alpha=alpha, beta=beta, gamma=gamma, nrad=nrad, mdis=mdis, vcheck=vcheck)
-
-        strength = max(strength, 0)
-        field = strength % 2
-        dh = strength <= 0 and not halfres
-
-        if strength > 0:
-            c = santiag_stronger(c, strength - 1, type)
-
-        w = c.width
-        h = c.height
-
-        if type == 'nnedi3':
-            return nnedi3(c, field=field, dh=dh)
-        elif type == 'eedi2':
-            if not dh:
-                c = c.resize.Point(w, h // 2, src_top=1 - field)
-            return c.eedi2.EEDI2(field=field)
-        elif type == 'eedi3':
-            sclip = nnedi3(c, field=field, dh=dh)
-            return eedi3(c, field=field, dh=dh, sclip=sclip)
-        elif type == 'sangnom':
-            if dh:
-                c = c.resize.Spline36(w, h * 2, src_top=-0.25)
-            return c.sangnom.SangNom(order=field + 1, aa=aa)
-        else:
-            raise vs.Error('santiag: unexpected value for type')
-
-    if not isinstance(c, vs.VideoNode):
-        raise vs.Error('santiag: this is not a clip')
-
-    type = type.lower()
-    typeh = type if typeh is None else typeh.lower()
-    typev = type if typev is None else typev.lower()
-
-    w = c.width
-    h = c.height
-    fwh = fw if strv < 0 else w
-    fhh = fh if strv < 0 else h
-
-    if strh >= 0:
-        c = santiag_dir(c, strh, typeh, fwh, fhh)
-    if strv >= 0:
-        c = santiag_dir(c.std.Transpose(), strv, typev, fh, fw).std.Transpose()
-
-    fw = fallback(fw, w)
-    fh = fallback(fh, h)
-    if strh < 0 and strv < 0:
-        c = c.resize.Spline36(fw, fh)
-    return c
+def santiag(*args, **kwargs):
+    raise vs.Error("havsfunc.santiag outdated. Use https://github.com/Irrational-Encoding-Wizardry/vs-aa instead.")
 
 
 def FixChromaBleedingMod(input: vs.VideoNode, cx: int = 4, cy: int = 4, thr: float = 4.0, strength: float = 0.8, blur: bool = False) -> vs.VideoNode:
