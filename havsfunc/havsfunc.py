7afb7b2e79b604465df20bcfb4a4dea7d78939f4
HolyWu <holywu@gmail.com>
2025-11-24 16:50:01 +0800

    fast_line_darken_mod: Cosmetics

--- havsfunc/havsfunc.py
+++ havsfunc/havsfunc.py
@@ -9,8 +9,8 @@ from vstools import (
     get_depth,
     get_peak_value,
     get_video_format,
+    get_y,
     join,
-    plane,
     scale_delta,
     vs,
 )
@@ -27,51 +27,49 @@ def fast_line_darken_mod(
     thinning: int = 0,
 ) -> vs.VideoNode:
     """
-    :param clip:        Clip to process.
-    :param strength:    Line darkening amount, 0-256. Represents the maximum amount that the luma will be reduced by,
-                        weaker lines will be reduced by proportionately less.
-    :param protection:  Prevents the darkest lines from being darkened. Protection acts as a threshold. Values range
-                        from 0 (no prot) to ~50 (protect everything).
-    :param luma_cap:    Value from 0 (black) to 255 (white), used to stop the darkening determination from being
-                        'blinded' by bright pixels, and to stop grey lines on white backgrounds being darkened. Any
-                        pixels brighter than luma_cap are treated as only being as bright as luma_cap. Lowering luma_cap
-                        tends to reduce line darkening. 255 disables capping.
-    :param threshold:   Any pixels that were going to be darkened by an amount less than threshold will not be touched.
-                        Setting this to 0 will disable it, setting it to 4 (default) is recommended, since often a lot
-                        of random pixels are marked for very slight darkening and a threshold of about 4 should fix
-                        them. Note if you set threshold too high, some lines will not be darkened.
-    :param thinning:    Optional line thinning amount, 0-256. Setting this to 0 will disable it, which gives a big speed
-                        increase. Note that thinning the lines will inherently darken the remaining pixels in each line
-                        a little.
+    Args:
+        clip: Clip to process.
+        strength: Line darkening amount, 0-256. Represents the maximum amount that the luma will be reduced by, weaker
+            lines will be reduced by proportionately less.
+        protection: Prevents the darkest lines from being darkened. Protection acts as a threshold. Values range from 0
+            (no prot) to ~50 (protect everything).
+        luma_cap: Value from 0 (black) to 255 (white), used to stop the darkening determination from being 'blinded' by
+            bright pixels, and to stop grey lines on white backgrounds being darkened. Any pixels brighter than luma_cap
+            are treated as only being as bright as luma_cap. Lowering luma_cap tends to reduce line darkening. 255
+            disables capping.
+        threshold: Any pixels that were going to be darkened by an amount less than threshold will not be touched.
+            Setting this to 0 will disable it, setting it to 4 (default) is recommended, since often a lot of random
+            pixels are marked for very slight darkening and a threshold of about 4 should fix them. Note if you set
+            threshold too high, some lines will not be darkened.
+        thinning: Optional line thinning amount, 0-256. Setting this to 0 will disable it, which gives a big speed
+            increase. Note that thinning the lines will inherently darken the remaining pixels in each line a little.
     """
     assert check_variable(clip, fast_line_darken_mod)
 
     fmt = get_video_format(clip)
     peak = get_peak_value(fmt)
 
-    if fmt.color_family == vs.RGB:
+    if fmt.color_family is vs.RGB:
         raise vs.Error("fast_line_darken_mod: RGB format is not supported")
 
-    if fmt.color_family != vs.GRAY:
+    if fmt.color_family is not vs.GRAY:
         clip_orig = clip
-        clip = plane(clip, 0)
+        clip = get_y(clip)
     else:
         clip_orig = None
 
-    # parameters
     Str = strength / 128
     lum = scale_delta(luma_cap, 8, clip)
     thr = scale_delta(threshold, 8, clip)
     thn = thinning / 16
 
-    # filtering
     exin = clip.std.Maximum(threshold=peak / (protection + 1)).std.Minimum()
-    thick = core.std.Expr([clip, exin], f"y {lum} < y {lum} ? x {thr} + > x y {lum} < y {lum} ? - 0 ? {Str} * x +")
+    thick = core.std.Expr([clip, exin], f"y {lum} min x {thr} + > x y {lum} min - 0 ? {Str} * x +")
     if thinning == 0:
         last = thick
     else:
         scale_127 = scale_delta(127, 8, clip)
-        diff = core.std.Expr([clip, exin], f"y {lum} < y {lum} ? x {thr} + > x y {lum} < y {lum} ? - 0 ? {scale_127} +")
+        diff = core.std.Expr([clip, exin], f"y {lum} min x {thr} + > x y {lum} min - 0 ? {scale_127} +")
         linemask = BlurMatrix.MEAN()(diff.std.Minimum().std.Expr(f"x {scale_127} - {thn} * {peak} +"))
         thin = core.std.Expr([clip.std.Maximum(), diff], f"x y {scale_127} - {Str} 1 + * +")
         last = thin.std.MaskedMerge(thick, linemask)
